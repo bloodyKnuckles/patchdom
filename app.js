029fff87e0d1ea5e63ebf869332d80e9fb2a81cc
bloodyKnuckles <treenopie@gmail.com>
2016-04-07 18:52:28 -0500

    Added async functions (via Promises) to routes.

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 var router = require('routes')()
+var XHR = require('xhr-promise-bare')
 
 var state = { 'clicks': 0, 'url': '/' }
 
 
 router.addRoute('*', function (data, routermatch) {
-  if ( 'url' === data.cmd ) {
-    state.url = data.url
-  }
-  console.log('all routes')
-  var rm = routermatch.next(data)
-  return rm.fn(data, rm)
-
+  return XHR('test.json')
+  /*
+  .then(function (response) {
+    var pp = Promise.defer()
+    setTimeout(function () {
+      pp.resolve(response)
+    }, 2000)
+    return pp.promise
+  }) //*/
+  .then(JSON.parse)
+  .then(function (testJSON) {
+    console.log(testJSON.test)
+    if ( 'url' === data.cmd ) {
+      state.url = data.url
+    }
+    console.log('all routes')
+    var rm = routermatch.next(data)
+    return rm.fn(data, rm)
+  })
 })
 
 router.addRoute("/(index.html)?", function (data, routermatch) {
-  if ( 'inc' ===  data.cmd ) {
-    state.clicks += 1
-  }
-  return Promise.resolve({
-    templates: '/index.html', // work in array option for layered templates
-    content: {'title': 'yes', '#count': state.clicks, 'button': {onclick: onclick, _html: 'hey'}}
+  return XHR('test.json')
+  .then(JSON.parse)
+  .then(function (testJSON) {
+    console.log(testJSON.test + ' hey')
+    if ( 'inc' ===  data.cmd ) {
+      state.clicks += 1
+    }
+    return Promise.resolve({
+      templates: '/index.html', // work in array option for layered templates
+      content: {'title': 'yes', '#count': state.clicks, 'button': {onclick: onclick, _html: 'hey'}}
+    })
+    function onclick (evt) {
+      console.log('clicked')
+      evt.preventDefault()
+      window.worker.postMessage({cmd: 'inc', url: '/'})
+    }
   })
-  function onclick (evt) {
-    console.log('clicked')
-    evt.preventDefault()
-    window.worker.postMessage({cmd: 'inc', url: '/'})
-  }
 })
 
 module.exports = router
